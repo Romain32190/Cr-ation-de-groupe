de25841a524a5da21a06c4dd1eba2ab93ad421a7
Romain32190 <romain.lafargue36@gmail.com>
2017-04-04 12:28:03 +0200

    Maj app.js, index.html et le style.css

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 $(document).ready(function(){
 
-
-var peuple = ["Jordy", "Florian", "Mouad", "Raphael", "Marco", "Dimitri", "Emilie", "Julien", "Franck", "Maxime", "Elodie", 
-					"Oceane", "Morel", "Gregory", "Romain"];
-
 var groupe = [];
 var random;
+var peuple = 0;
 
-for(var n = 0; n < Groupe; n++){
-}
+var personne = ["Jordy", "Florian", "Morel", 
+				"Raphael", "Marco", "Dimitri", 
+				"Emilie", "Julien", "Franck", 
+				"Maxime", "Elodie", "Oceane", 
+				"Mouad", "Gregory", "Romain"];
 
-for(var i = peuple.length; i > 0; i--){
+
+var Peuple = function(){
+	for(var i = personne.length; i > 0; i--){
 		random = Math.floor(Math.random() * i);
-		groupe.push(peuple[random]);
-		peuple.splice(random, 1);
-		console.log(groupe);
+		groupe.push(personne[random]);
+		personne.splice(random, 1);
+	}
+};
+
+var AjouterTh = function(){
+	peuple = parseInt(prompt("Rentrez le nombre de groupe que vous voulez?"));
+	for(var o = 0; o < parseInt(peuple); o++){
+	}
+}; 
+
 	
-}
-});
+var tableaupeuple = function(){
+
+	$('tbody').append("<tr></tr>");
+	for( var j = 1 ; j <= groupe.length; j++ ){
+		$("tr").last().append("<td>" + groupe[j - 1] + "</td>");
+			if(j % peuple === 0) {
+				$('tbody').append("<tr></tr>");
+
+			}	
+	};
+};
+
+	
+	$('#button').click(function(){
+		Peuple();
+		AjouterTh();
+		tableaupeuple();
+	});  
+
+	
+}); 
 
     
